744c4ccf61bda7619e58fc00bf248705a9d81176
okerivy <okerivy@163.com>
2017-03-07 18:33:42 +0800

    Leetcode 初始化

---
+++ AlgorithmLeetCode/AlgorithmLeetCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AlgorithmLeetCode
+//
+//  Created by okerivy on 2017/3/7.
+//  Copyright © 2017年 okerivy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
